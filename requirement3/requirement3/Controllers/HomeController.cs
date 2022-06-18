3e42d4672c21ccfde05cab745cfe466e519568ba
kum jie wern <kjwern@gmail.com>
2022-06-18 16:48:40 +0800

    Initial commit: 1. To create post api to submit name and annualpay and receive payslip info 2. To store time, name, annualpay, monthlytax to database table [payslip]

---
+++ requirement3/requirement3/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using requirement3.Models;
+using System.Diagnostics;
+
+namespace requirement3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [Route("~/api/submitpayslip")]
+        [HttpPost]
+        public string SubmitPayslip(PayslipRequest req)
+        {
+            if (ModelState.IsValid)
+            {
+                //var monthlyData = generate_monthly_payslip(name, annualpay);
+
+                PayslipResponse response = new PayslipResponse();
+                response.generate_monthly_payslip(req.Name, req.AnnualPay);
+
+
+                var jsonResponse = JsonConvert.SerializeObject(response);
+                return jsonResponse;
+
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+
+    }
+}
